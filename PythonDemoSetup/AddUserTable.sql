25a02bdca19f99d067e35845cb97d57c6ef04494
Larry Clement <lclement@calbaptist.edu>
2023-03-16 14:28:00 -0700

    After update to PythonDemoSetup

--- PythonDemoSetup/AddUserTable.sql
+++ PythonDemoSetup/AddUserTable.sql
@@ -5,12 +5,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
 	username		VARCHAR(50)		PRIMARY KEY,
-    admin			BOOLEAN
+    password		VARCHAR(50)
 );
 
 INSERT INTO users VALUES
-('haki', FALSE),
-('ran', TRUE),
-('joe', FALSE),
-('sam', FALSE),
-('admin', TRUE);
+('haki', 'password1'),
+('ran', 'password2'),
+('joe', 'password3'),
+('sam', 'password4'),
+('admin', 'password5');
